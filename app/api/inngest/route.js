d9c67dd9c69a5d86d8ec639abd0f609b50a506f1
Rishi chauhan <rishikeshc216@gmail.com>
2026-04-06 16:04:13 +0530

    test db

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -2,6 +2,14 @@ export const runtime = "nodejs";
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
 import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/inngest/functions";
+/// app/api/test-db/route.js
+
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return Response.json({ users });
+}
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
