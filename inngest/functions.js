8bd6a7b040079e0e7b876b2a62908ac21718af1e
Rishi chauhan <rishikeshc216@gmail.com>
2026-04-06 17:29:01 +0530

    safe inngest functions

--- inngest/functions.js
+++ inngest/functions.js
@@ -10,10 +10,12 @@ export const syncUserCreation = inngest.createFunction(
   async ({ event }) => {
     const { data } = event;
 
+    const email= data.email_addresses[0].email_address || null;
+
     await prisma.user.create({
       data: {
         id: data.id,
-        email: data.email_addresses[0].email_address,
+        email: email,
         name: `${data.first_name} ${data.last_name}`,
         image: data.image_url,
       },
@@ -28,12 +30,14 @@ export const syncUserUpdation = inngest.createFunction(
     triggers: [{ event: "clerk/user.updated" }],
   },
   async ({ event }) => {
-    const { data } = event;
+    const { data } = event.data;
+
+    const email = data.email_addresses[0].email_address || null;
 
     await prisma.user.update({
       where: { id: data.id },
       data: {
-        email: data.email_addresses[0].email_address,
+        email: email,
         name: `${data.first_name} ${data.last_name}`,
         image: data.image_url,
       },
@@ -48,7 +52,7 @@ export const syncUserDeletion = inngest.createFunction(
     triggers: [{ event: "clerk/user.deleted" }],
   },
   async ({ event }) => {
-    const { data } = event;
+    const { data } = event.data;
 
     await prisma.user.delete({
       where: { id: data.id },
